e71dc730628e88edc9741e216c1872ddcd9dc4a0
Patrick Kowalik <patrick@Patricks-Air.fritz.box>
2022-05-23 20:23:40 +0200

    First Iteration - Simple Blockchain

---
+++ Block.py
@@ -0,0 +1,37 @@
+import hashlib
+
+class Block:
+    prev_hash = None
+    nonce = -1
+    data = None
+    current_hash = None
+
+    def __init__(self, prev_hash, data):
+        self.prev_hash = prev_hash
+        self.data = data
+
+    def __repr__(self):
+        return "Block: {0} (prev. {1})".format(
+            self.current_hash,
+            self.prev_hash
+        )
+    
+    @property
+    def block_string(self):
+        return "{0}-{1}-{2}".format(
+            self.prev_hash,
+            self.data,
+            self.nonce
+        )
+    
+    def hash(self):
+        return hashlib.sha256(bytes(self.block_string,"utf-8")).hexdigest()
+
+    def mine(self, difficulty):
+        found = False
+        while not found:
+            self.nonce += 1
+            self.current_hash = self.hash()
+            if self.current_hash[0:difficulty] == "0" * difficulty:
+                found = True
+            
---
+++ Blockchain.py
@@ -0,0 +1,23 @@
+from Block import Block
+
+class Blockchain:
+    blocks = []
+    difficulty = 4
+
+    def __init__(self):
+        pass
+
+    def add(self, block):
+        block.mine(self.difficulty)
+        self.blocks.append(block)
+    
+    @property
+    def last_block(self):
+        return self.blocks[-1] if len(self.blocks) else None
+    
+    def build_block(self, data):
+        if self.last_block:
+            block = Block(self.last_block.current_hash, data)
+            self.add(block)
+        else:
+            raise Exception("Missing Genesis block")

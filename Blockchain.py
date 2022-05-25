cccdbdacbd08cf9f9a4d4b800e678ac488cf28d9
Patrick0585 <patrick0585@googlemail.com>
2022-05-25 21:27:18 +0200

    Blockchain Klasse überarbeitet

--- Blockchain.py
+++ Blockchain.py
@@ -2,25 +2,61 @@
     Blockchain module
 '''
 from Block import Block
+from time import time
 
 class Blockchain:
-    blocks = []
-    difficulty = 4
 
     def __init__(self):
-        pass
+        self.pending_transactions = []
+        self.chain = []
+        self.difficulty = 4
+        self.create_genesis_block()
 
-    def add(self, block):
-        block.mine(self.difficulty)
-        self.blocks.append(block)
-    
+    def create_genesis_block(self):
+        genesis_block = Block(0, "Genesis-Block", time(), 0)
+        genesis_block.hash = genesis_block.compute_hash()
+        self.chain.append(genesis_block)
+
     @property
     def last_block(self):
-        return self.blocks[-1] if len(self.blocks) else None
+        return self.chain[-1]
+
+    def mine(self):
+
+        if not self.pending_transactions:
+            return False
+
+        last_block = self.last_block
+        block = Block(index=last_block.index + 1,
+                    transactions=self.pending_transactions,
+                    timestamp=time(),
+                    previous_hash=last_block.hash
+        )
+
+        block.proof_of_work(difficulty=self.difficulty)
+
+        if last_block.hash != block.previous_hash:
+            print(last_block.hash)
+            print(block.previous_hash)
+            return False
+        
+        if not self.validate_proof(block):
+            return False
+        
+        self.chain.append(block)
+
+
+    def validate_proof(self, block):
+        print(block.hash)
+        print(block.compute_hash())
+        print(block.compute_hash())
+        return (block.hash.startswith('0' * self.difficulty))
+
+    def new_transaction(self, sender, recipient, amount):
+        transaction = {
+            'sender' : sender,
+            'recipient' : recipient,
+            'amount' : amount
+        }
+        self.pending_transactions.append(transaction)
     
-    def build_block(self, data):
-        if self.last_block:
-            block = Block(self.last_block.current_hash, data)
-            self.add(block)
-        else:
-            raise Exception("Missing Genesis block")
